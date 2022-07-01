ca30938ebd2c7127ed0b4a6a77ad7a573d3884a2
Anx450z <anx450z@gmail.com>
2022-07-01 23:44:43 +0530

    Add functions and some script - Chapter 4

--- hello.js
+++ hello.js
@@ -1,4 +1,22 @@
+
+const print = (paper) => {
+  console.log(paper);
+}
+puts = print;
+
+
 console.log("hello, world!","this as well")
 
-a = "A man, a plan, a canal, Panama"
+let a = "A man, a plan, a canal, Panama"
 console.log(a.slice(", "));
+
+let now = new Date();
+
+print(now);
+puts(now);
+
+let moonlanding = new Date("July 20, 1969 20:18");
+
+puts(now-moonlanding);
+
+puts(now.getDay()) // JS uses sunday as first day, and months are offset by 1, i.e Jan = 0

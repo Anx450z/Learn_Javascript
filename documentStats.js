ae588bc75956e91cbd7ed2394b17527fe1fb4ee8
Anx450z <anx450z@gmail.com>
2022-07-02 22:04:48 +0530

    Completed document stat - version 1

--- documentStats.js
+++ documentStats.js
@@ -6,26 +6,33 @@
 //                                             //
 /////////////////////////////////////////////////
 
-// Breakup text to max 80 character
+// Breakup text to max characters
 const brakeUpText = (text)=> {
-  let textLength = text.length;
-  if (textLength > 40){
+  const textLength = text.length;
+  const padding = 4;
+  const lineLimit = 30; // works from range 21+
+  if (textLength > lineLimit){
     textSegments = text.split(" ");
     let segmentLengthSum = 0;
     let next = 0;
     let line = "";
-  
-    for (let i=0; i <= textSegments.length; i++){
+    let i=0;
+    for (i=0; i < textSegments.length; i++){
       segmentLengthSum += textSegments[i].length;
-      line += textSegments.splice(next,i).join(" ");
-      console.log(segmentLengthSum);
-      if(segmentLengthSum > 10){
-        console.log(line + "\n");
+      if(segmentLengthSum > lineLimit){
+        newLine = "//"+" ".repeat(padding/2)+textSegments.slice(next,i).join(" ");
+        newLineSpace = " ".repeat(lineLimit*2 - newLine.length -2) + "//";
+        line += newLine + newLineSpace + "\n";
         segmentLengthSum = 0;
         next = i;
-        line = "";
       }
     }
+    newLine = "//"+" ".repeat(padding/2)+textSegments.slice(next,i).join(" ");
+    newLineSpace = " ".repeat(lineLimit*2 - newLine.length -2) + "//";
+    line += newLine + newLineSpace;
+    console.log("//".repeat(lineLimit));
+    console.log(line);
+    console.log("//".repeat(lineLimit));
   }
 }
 

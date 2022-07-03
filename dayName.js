1b7f5863411727e0d9f211e77ba75108abd7a3ba
Anx450z <anx450z@gmail.com>
2022-07-03 23:24:39 +0530

    Add 2 palindrome methods

--- dayName.js
+++ dayName.js
@@ -8,3 +8,22 @@ const greetings = (date)=>{
   let day = dayName(date);
   return `Hello world!, Happy ${day}.`;
 }
+
+const palindrome = (word) =>{
+  reverseWord = word.split("").reverse().join("");
+  if (reverseWord.toLowerCase() === word.toLowerCase()){
+    return 1;
+  }
+  return 0;
+}
+
+// Another way of palindrome
+
+const palindrome2 = (word) =>{
+  return word.toLowerCase() === word.split("").reverse().join("").toLowerCase();
+}
+
+console.log(palindrome("ankur")) ;
+console.log(palindrome("racecar")) ;
+console.log(palindrome2("Racecar")) ;
+console.log(palindrome2("Ankur")) ;

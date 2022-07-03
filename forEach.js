aad0784a13199c1e271ab180c9e93396bde28d37
Anx450z <anx450z@gmail.com>
2022-07-03 23:35:42 +0530

    Add seperate functions files and use for each loop

---
+++ forEach.js
@@ -0,0 +1,16 @@
+
+a = [23,44,44,67,67,324,87];
+
+a.forEach(element => {
+  console.log(element);
+});
+
+a.forEach(function(element){
+  console.log(element);
+});
+
+let b = ["text", true, 45];
+
+b.forEach(element=>{
+  console.log(element);
+});
---
+++ palindrome.js
@@ -0,0 +1,26 @@
+
+const palindrome = (word) =>{
+  reverseWord = word.split("").reverse().join("");
+  if (reverseWord.toLowerCase() === word.toLowerCase()){
+    return 1;
+  }
+  return 0;
+}
+
+// Another way of palindrome
+
+const palindrome2 = (word) =>{
+  return word.toLowerCase() === word.split("").reverse().join("").toLowerCase();
+}
+
+console.log(palindrome("ankur")) ;
+console.log(palindrome("racecar")) ;
+console.log(palindrome2("Racecar")) ;
+console.log(palindrome2("Ankur")) ;
+
+const reverse = (word) =>{
+  return Array.from(word).reverse().join("")
+}
+
+console.log(reverse("😂❤️😂"));
+console.log(reverse("😂❤️🌐😂"));

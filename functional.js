bf9e3d8d63a51644313a6c1c7aad01844e7ab29f
Anx450z <anx450z@gmail.com>
2022-07-04 19:03:24 +0530

    Add reduce

--- functional.js
+++ functional.js
@@ -52,3 +52,49 @@ const filter2 = (elements)=> {
 
 console.log(filter1(states));
 console.log(filter2(states));
+
+// Reduce
+// sum: Imperative solution
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+const imperativeSum= (elements)=>{
+  let sum = 0;
+  numbers.forEach(number =>{
+    sum += number;
+  })
+  return sum;
+}
+console.log(imperativeSum(numbers));
+
+// using reduce
+numbers.reduce((sum, number) => { // takes 2 arguments : 1- accumulator, 2- array element
+  sum += number;
+  return sum;
+}, 0);// 0 is default value of sum
+
+// second method
+
+const functionalSum = (elements)=>{
+  return elements.reduce((sum, element) =>{return sum += element;});// double return
+}
+console.log(functionalSum(numbers));
+
+// example 2 for reduce
+
+// lengths: Imperative solution
+function imperativeLengths(elements) {
+  let lengths = {};
+  elements.forEach(function(element) {
+    lengths[element] = element.length;
+  });
+  return lengths;
+}
+console.log(imperativeLengths(states));
+
+// functional method
+const functionalLengths = (elements)=>{
+  return elements.reduce((lengths, element) => {
+    lengths[element] = element.length;
+    return lengths;
+  }, {});
+}
+console.log(functionalLengths(states));

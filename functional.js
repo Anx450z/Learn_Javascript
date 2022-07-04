566b0fb8a6084801da32f17e5d6b9dbf8a655cf9
Anx450z <anx450z@gmail.com>
2022-07-04 12:53:03 +0530

    Add filter in functional programing

--- functional.js
+++ functional.js
@@ -8,12 +8,35 @@ const imperativeUrls = (elements)=> {
   });
   return urls;
 }
-
 console.log(imperativeUrls(states));
 
 // using functional version : map
 const functionalUrls = (elements)=>{
   return elements.map(element => element.toLowerCase().split(/\s+/).join("-"));
 }
-
 console.log(functionalUrls(states));
+
+// exercise
+const urlsFriendly = (elements)=>{
+  return elements.map(element => `https://example.com/${element.toLowerCase().split(/\s+/).join("-")}`);
+}
+console.log(urlsFriendly(states));
+
+// Filter
+// singles: Imperative form
+const imperativeString =(elements)=>{
+  let singles = [];
+  elements.forEach(element => {
+    if (element.split(/\s+/).length === 1){
+      singles.push(element);
+    }
+  });
+  return singles;
+}
+console.log(imperativeString(states));
+
+// using filter
+const functionalString= (elements)=>{
+  return elements.filter(element => element.split(/\s+/).length === 1); //works on true
+}
+console.log(functionalString(states));

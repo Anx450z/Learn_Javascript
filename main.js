0e4d7cac886930baf0284a9fe6c7db33ac643f5e
Anx450z <anx450z@gmail.com>
2022-07-08 23:57:24 +0530

    add function and buttons into page and use event listners

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 let Phrase = require("mhartl-palindrome");
 
-let string = prompt("Plase enter your string for palindrome testing: ");
-let phrase = new Phrase(string);
+function palindromeTester() {
+  let string = prompt("Plase enter your string for palindrome testing: ");
+  let phrase = new Phrase(string);
 
-if(phrase.palindrome()){
-  alert(`"${phrase.content}" is a palindrome`);
-}else{
-  alert(`"${phrase.content}" is not a palindrome`);
+  if(phrase.palindrome()){
+    alert(`"${phrase.content}" is a palindrome`);
+  }else{
+    alert(`"${phrase.content}" is not a palindrome`);
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+  let form = document.querySelector("#palindrome-tester");
+  form.addEventListener("submit", function(){
+    palindromeTester();
+  });
+});

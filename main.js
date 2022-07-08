5b52bd2b7c87c44dbad9fa34a0e511558a6890c7
Anx450z <anx450z@gmail.com>
2022-07-08 14:58:41 +0530

    add promp and prevent Jekyll build

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let Phrase = require("mhartl-palindrome");
 
-alert(new Phrase("Madam, I'm Adam.").palindrome());
+let string = prompt("Plase enter your string for palindrome testing: ");
+let phrase = new Phrase(string);
+
+if(phrase.palindrome()){
+  alert(`"${phrase.content}" is a palindrome`);
+}else{
+  alert(`"${phrase.content}" is not a palindrome`);
+}
